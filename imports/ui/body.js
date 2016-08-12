74d882f95dc47959b76cf65933a56472be695967
markstanley <mark.stanley@coraledge.co.uk>
2016-08-12 09:23:03 +0100

    First commit of work in progress venues app

---
+++ imports/ui/body.js
@@ -0,0 +1,95 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import { Venues } from '../api/venues.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+ 
+import './venue.js';
+import './body.html';
+ 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+  Meteor.subscribe('venues');
+});
+
+Template.VenueList.helpers({
+  venues() {
+    //console.log("body.js venuesssummary helper");
+    return Venues.find();
+  },
+  incompleteCount() {
+    return Venues.find().count();
+  },
+});
+
+Template.Venue.helpers({
+  thisVenue() {
+    const venueId=FlowRouter.getParam("venueId");
+    // console.log(venueId);
+    return Venues.findOne({"_id": venueId});
+  }
+});
+
+Template.EditVenue.helpers({
+  thisVenue() {
+    const venueId=FlowRouter.getParam("venueId");
+    console.log(venueId);
+//    return Venues.find({"_id": "ZzwCsxZHFRQDiXtiF"});
+    return Venues.findOne({"_id": venueId});
+  }
+});
+
+Template.AddVenue.events({
+  'submit .add-venue'(event) {
+    //console.log("Save me!")
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const venueName = target.venueName.value;
+    const venueDescription = target.venueDescription.value;
+    const venueLatitude = target.venueLatitude.value;
+    const venueLongitude = target.venueLongitude.value;
+    const venueOpeningHours = target.venueOpeningHours.value;
+    const venueWebsite = target.venueWebsite.value;
+    const venueBooking = target.venueBooking.value;
+    const venueNews = target.venueNews.value;
+ 
+    // Insert a venue into the collection
+    Meteor.call('venues.insert', venueName, venueDescription, 
+      venueLatitude, venueLongitude, venueOpeningHours, 
+      venueWebsite, venueBooking, venueNews);
+    // Clear form
+    //target.text.value = '';
+    window.location.assign("/");
+  },
+});
+
+Template.EditVenue.events({
+  'submit .edit-venue'(event) {
+    //console.log("Save me!")
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const venueId = this._id;
+    const venueName = target.venueName.value;
+    const venueDescription = target.venueDescription.value;
+    const venueLatitude = target.venueLatitude.value;
+    const venueLongitude = target.venueLongitude.value;
+    const venueOpeningHours = target.venueOpeningHours.value;
+    const venueWebsite = target.venueWebsite.value;
+    const venueBooking = target.venueBooking.value;
+    const venueNews = target.venueNews.value;
+ 
+    // Insert a venue into the collection
+    Meteor.call('venues.update', venueId, venueName, venueDescription, 
+      venueLatitude, venueLongitude, venueOpeningHours, 
+      venueWebsite, venueBooking, venueNews);
+    // Clear form
+    //target.text.value = '';
+    window.location.assign("/venue/" + this._id);
+  },
+});
---
+++ imports/ui/router.js
@@ -0,0 +1,42 @@
+FlowRouter.route('/', {
+  action: function() {
+    BlazeLayout.render("mainLayout", {content: "VenueList"});
+  }
+});
+
+FlowRouter.route('/addvenue', {
+  action: function() {
+    console.log("AddVenue template");
+    BlazeLayout.render("mainLayout", {content: "AddVenue"});
+  }
+});
+
+FlowRouter.route('/editvenue/:venueId', {
+  action(params, queryParams) {
+    BlazeLayout.render("mainLayout", {content: "EditVenue"});
+  }
+});
+
+FlowRouter.route('/venueevents/:venueId', {
+  action(params, queryParams) {
+    BlazeLayout.render("mainLayout", {content: "VenueEvents"});
+  }
+});
+
+FlowRouter.route('/venue/:venueId', {
+  action(params, queryParams) {
+    BlazeLayout.render("mainLayout", {content: "Venue"});
+  }
+});
+
+FlowRouter.route('/addevent/:venueId', {
+  action(params, queryParams) {
+    BlazeLayout.render("mainLayout", {content: "AddEvent"});
+  }
+});
+
+FlowRouter.route('/editevent/venueId', {
+  action(params, queryParams) {
+    BlazeLayout.render("mainLayout", {content: "EditEvent"});
+  }
+});
